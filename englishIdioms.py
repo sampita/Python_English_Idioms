384a26c7d10173c94c074ce2661d658d648442ac
Sam Pita <samanthakpita@gmail.com>
2020-01-14 15:16:41 -0600

    finished practice

--- englishIdioms.py
+++ englishIdioms.py
@@ -1,6 +1,3 @@
-Practice: English Idioms
-Create a new Python file and paste the following code into it. At the end write a for in loop to produce the output below.
-
 idioms = {
     "Penny": ["A", "penny", "for", "your", "thoughts"],
     "Injury": ["Add", "insult", "to", "injury"],
@@ -13,16 +10,22 @@ idioms = {
     "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
     "Lucifer": ["Speak", "of", "the", "devil"],
 }
-Make sure you join the strings
 
-Output
-Penny: A penny for your thoughts
-Injury: Add insult to injury
-Moon: Once in a blue moon
-Grape: I heard it through the grapevine
-Murder: Kill two birds with one stone
-Limbs: It costs an arm and a leg
-Grain: Take what someone says with a grain of salt
-Fences: I'm on the fence about it
-Sheep: Pulled the wool over his eyes
-Lucifer: Speak of the devil
+# print(idioms.items())
+# for (title, idiom) in idioms.items
+
+for items in idioms.items():
+    print(f"{items[0]}: {' '.join(items[1])}")
+
+
+# Output
+# Penny: A penny for your thoughts
+# Injury: Add insult to injury
+# Moon: Once in a blue moon
+# Grape: I heard it through the grapevine
+# Murder: Kill two birds with one stone
+# Limbs: It costs an arm and a leg
+# Grain: Take what someone says with a grain of salt
+# Fences: I'm on the fence about it
+# Sheep: Pulled the wool over his eyes
+# Lucifer: Speak of the devil
